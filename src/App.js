f35f8d3a3d94ac0a35edffdd7a626d3c3b161094
tony trinh <tony.w.trinh3@gmail.com>
2020-11-13 15:14:54 -0800

    rock paper scissors lizard spock works

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "sass/main.scss";
-import {ROCK,PAPER,SCISSORS} from "utilities/types";
-import getRndInteger from 'utilities/getRndInteger';
+import { ROCK, PAPER, SCISSORS, SPOCK, LIZARD } from "utilities/types";
+import getRndInteger from "utilities/getRndInteger";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.resultsBanner = React.createRef();
     this.state = {
       userChoice: null,
       cpuChoice: null,
@@ -13,93 +14,113 @@ class App extends React.Component {
       resultsBanner: null,
       rulesModal: false,
       rounds: 0,
-      cpuOption: [ROCK, PAPER, SCISSORS],
+      cpuOption: [ROCK, PAPER, SCISSORS, SPOCK, LIZARD],
       gameLogic: {
-        "ROCK": {
-          wins: SCISSORS,
-          loses: PAPER,
+        ROCK: {
+          wins: [SCISSORS, LIZARD],
+          loses: [PAPER, SPOCK],
         },
-        "PAPER": {
-          wins: ROCK,
-          loses: SCISSORS,
+        PAPER: {
+          wins: [ROCK, SPOCK],
+          loses: [SCISSORS, LIZARD],
         },
-        "SCISSORS": {
-          wins: PAPER,
-          loses: ROCK,
+        SCISSORS: {
+          wins: [PAPER, LIZARD],
+          loses: [ROCK, SPOCK],
+        },
+        SPOCK: {
+          wins: [SCISSORS, ROCK],
+          loses: [PAPER, LIZARD],
+        },
+        LIZARD: {
+          wins: [SPOCK, PAPER],
+          loses: [SCISSORS, ROCK],
         },
       },
     };
   }
 
-  componentDidMount(){
+  componentDidMount() {
     // this.getCPUChoice();
   }
 
-  async componentDidUpdate(prevProps, prevState){
-    const { userChoice, cpuChoice,gameLogic,rounds } = this.state;
+  async componentDidUpdate(prevProps, prevState) {
+    const { userChoice, gameLogic, rounds, userScore } = this.state;
 
-    let result = null;
-   
-    if(prevState.rounds !== rounds){
+    if (prevState.rounds !== rounds) {
       let newCPUChoice = await this.getCPUChoice();
 
-      if(newCPUChoice === userChoice){
-        result = "draw"
-        console.log(result)
-      } 
-      else if (gameLogic[newCPUChoice].loses === userChoice){
-        result = "Player wins!"
-        console.log(result)
-      } else {
-        result = "Player loses :("
-        console.log(result)
+      if (newCPUChoice === userChoice) {
+        return this.setState({
+          resultsBanner: "draw",
+          userScore: userScore + 0,
+        });
+      }
+
+      for (let i = 0; i < gameLogic[newCPUChoice].loses.length; i++) {
+        if (gameLogic[newCPUChoice].loses[i] === userChoice) {
+          return this.setState({
+            resultsBanner: "Player wins!",
+            userScore: userScore + 1,
+          });
+        }
       }
 
       this.setState({
-        resultsBanner: result
-      })
+        resultsBanner: "Player loses :(",
+        userScore: userScore - 1,
+      });
     }
   }
 
   getCPUChoice() {
-    return new Promise(resolve =>{
-      const index = getRndInteger(0,2);
+    //in order to make sure we get cpu results first then render game results and score second
+    return new Promise((resolve) => {
+      const index = getRndInteger(0, 4);
       const cpuChoice = this.state.cpuOption[index];
       this.setState({
-        cpuChoice: cpuChoice
-      })
-      setTimeout(()=>{
-        resolve(this.state.cpuChoice)
-      },200)
-    })
-   
+        cpuChoice: cpuChoice,
+      });
+      setTimeout(() => {
+        resolve(this.state.cpuChoice);
+      }, 200);
+    });
   }
 
-  getUserChoice=(choice) =>{
+  getUserChoice = (choice) => {
     this.setState({
       userChoice: choice,
-      rounds: this.state.rounds + 1
-    })
-  }
+      rounds: this.state.rounds + 1,
+    });
+  };
+
+  // componentWillUnmount () {
+  //   //TODO: need to clear result before each round
+  //   this.resultsBanner = this.resultsBanner.destroy();
+  // }
 
   //TODO: RENDERING OF BUTTON COULD BE DONE THROUGH A COMPONENT
   render() {
     return (
       <div className="App">
-        {this.state.cpuChoice}
-        <br/>
-        <button onClick={()=>this.getUserChoice(ROCK)}>Rock</button>
-        <button onClick={()=>this.getUserChoice(PAPER)}>Paper</button>
-        <button onClick={()=>this.getUserChoice(SCISSORS)}>Scissor</button>
-        <br/>
-        {this.state.userChoice}
-        <br/>
-        {this.state.resultsBanner}
+        {`CPU chooses: ${this.state.cpuChoice}`}
+        <br />
+        <button onClick={() => this.getUserChoice(ROCK)}>Rock</button>
+        <button onClick={() => this.getUserChoice(PAPER)}>Paper</button>
+        <button onClick={() => this.getUserChoice(SCISSORS)}>Scissor</button>
+        <button onClick={() => this.getUserChoice(SPOCK)}>SPOCK</button>
+        <button onClick={() => this.getUserChoice(LIZARD)}>LIZARD</button>
+        <br />
+        {`User chooses: ${this.state.userChoice}`}
+        <br />
+        <div className="aewf" ref={this.resultsBanner}>
+          {this.state.resultsBanner}
+        </div>
+        <br />
+        {this.state.userScore}
       </div>
-    )
+    );
   }
 }
 
 export default App;
-
-
--- src/utilities/types.js
+++ src/utilities/types.js
@@ -1,3 +1,5 @@
 export const ROCK = "ROCK";
 export const PAPER = "PAPER";
 export const SCISSORS = "SCISSORS";
+export const SPOCK = "SPOCK";
+export const LIZARD = "LIZARD";
