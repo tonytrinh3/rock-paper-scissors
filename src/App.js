c59c5589fa85bb0969a706b430bd6a34b045eea5
tony trinh <tony.w.trinh3@gmail.com>
2020-11-13 12:19:00 -0800

    rendered user choice and cpu choice

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "sass/main.scss";
 import {ROCK,PAPER,SCISSORS} from "utilities/types";
+import getRndInteger from 'utilities/getRndInteger';
 
 class App extends React.Component {
   constructor(props) {
@@ -12,30 +13,59 @@ class App extends React.Component {
       rulesModal: false,
       cpuOption: [ROCK, PAPER, SCISSORS],
       gameLogic: {
-        ROCK: {
+        "ROCK": {
           wins: SCISSORS,
           loses: PAPER,
-          draw: ROCK
         },
-        PAPER: {
+        "PAPER": {
           wins: ROCK,
           loses: SCISSORS,
-          draw: PAPER
         },
-        SCISSORS: {
+        "SCISSORS": {
           wins: PAPER,
           loses: ROCK,
-          draw: SCISSORS
         },
       },
     };
   }
 
-  getCPUChoice() {}
+  componentDidMount(){
+    this.getCPUChoice()
+  }
 
+  getCPUChoice() {
+
+    const index = getRndInteger(0,2);
+
+    const cpuChoice = this.state.cpuOption[index];
+
+    this.setState({
+      cpuChoice: cpuChoice
+    })
+
+  }
+
+  getUserChoice=(choice) =>{
+    this.setState({
+      userChoice: choice
+    })
+  }
+
+  //TODO: RENDERING OF BUTTON COULD BE DONE THROUGH A COMPONENT
   render() {
-    return <div className="App">hi</div>;
+    return (
+      <div className="App">
+        {this.state.cpuChoice}
+        <br/>
+        <button onClick={()=>this.getUserChoice(ROCK)}>Rock</button>
+        <button onClick={()=>this.getUserChoice(PAPER)}>Paper</button>
+        <button onClick={()=>this.getUserChoice(SCISSORS)}>Scissor</button>
+        {this.state.userChoice}
+      </div>
+    )
   }
 }
 
 export default App;
+
+
