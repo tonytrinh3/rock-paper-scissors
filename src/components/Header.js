e6561f84c3e119bada31b9a1ed8ec3457b7a1451
tony trinh <tony.w.trinh3@gmail.com>
2020-11-14 12:55:55 -0800

    got rules button stuck in the bottom

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import logo from "assets/img/logo-bonus.svg";
 //TODO - REDUCE HEADER WIDTH TO BE LIKE PIC
 const Header = (props) => {
   return (
-    <div className="header">
+    <header className="header">
       <img className = "header__logo" src={logo} alt="logo" />
 
       <div className="header__score-card">
@@ -12,7 +12,7 @@ const Header = (props) => {
           <p className="header__score-card__score">{props.score}</p>
       </div>
   
-    </div>
+    </header>
   );
 };
 
