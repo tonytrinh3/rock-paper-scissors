9e8c7e419ca0d426d57c0c36ea416a56ab91b0a9
tony trinh <tony.w.trinh3@gmail.com>
2020-11-13 17:52:37 -0800

    got the circles ...

--- src/components/UserSelection.js
+++ src/components/UserSelection.js
@@ -9,23 +9,41 @@ import pentagon from "assets/img/bg-pentagon.svg";
 const UserSelection = () => {
   return (
     <div className="user-selection">
-      <div className="game-piece">
-        <img className="game-piece__img" src={ROCK} alt="ROCK" />
+      <div class="outer-circle outer-circle__rock">
+        <div class="inner-circle inner-circle__rock"></div>
       </div>
-      <div className="game-piece">
-        <img className="game-piece__img" src={PAPER} alt="PAPER" />
+      <div class="outer-circle outer-circle__paper">
+        <div class="inner-circle inner-circle__paper"></div>
       </div>
-      <div className="game-piece">
-        <img className="game-piece__img" src={SCISSORS} alt="SCISSORS" />
+      <div class="outer-circle outer-circle__scissors">
+        <div class="inner-circle inner-circle__scissors"></div>
       </div>
-      <div className="game-piece">
-        <img className="game-piece__img" src={LIZARD} alt="LIZARD" />
+      <div class="outer-circle outer-circle__spock">
+        <div class="inner-circle inner-circle__spock"></div>
       </div>
-      <div className="game-piece">
-        <img className="game-piece__img" src={SPOCK} alt="SPOCK" />
+      <div class="outer-circle outer-circle__lizard">
+        <div class="inner-circle inner-circle__lizard"></div>
       </div>
     </div>
   );
 };
 
 export default UserSelection;
+
+{
+  /* <div className="game-piece game-piece--rock">
+<img className="game-piece__img" src={ROCK} alt="ROCK" />
+</div>
+<div className="game-piece">
+<img className="game-piece__img" src={PAPER} alt="PAPER" />
+</div>
+<div className="game-piece">
+<img className="game-piece__img" src={SCISSORS} alt="SCISSORS" />
+</div>
+<div className="game-piece">
+<img className="game-piece__img" src={LIZARD} alt="LIZARD" />
+</div>
+<div className="game-piece">
+<img className="game-piece__img" src={SPOCK} alt="SPOCK" />
+</div> */
+}
